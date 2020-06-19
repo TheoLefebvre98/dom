a2731d218656c2aac695ee764a88a89fd7487114
Theo <lefebvre.theo98@gmail.com>
2020-06-19 16:17:24 +0200

    01-02-03 done

--- 01-modelise.js
+++ 01-modelise.js
@@ -1,3 +1,134 @@
 let document = {
-    // CODE HERE
+    nodes: [
+        {
+            html: {
+                attributes: {
+                    lang: 'en'
+                },
+                nodes: [
+                    {
+                        head: {
+                            nodes: [
+                                {
+                                    meta: {
+                                        attributes: { charset: "UTF-8" }
+                                    }
+                                },
+                                {
+                                    meta: {
+                                        attributes: { httpEquiv: "X-UA-Compatible" }
+                                    }
+                                },
+                                {
+                                    title: { text: "Mon super titre" }
+                                }
+                            ]
+                        },
+                        body: {
+                            nodes: [
+                                {
+                                    header: {
+                                        nodes: [
+                                            {
+                                                ul: {
+                                                    nodes: [
+                                                        {
+                                                            li: {
+                                                                nodes: [
+                                                                    {
+                                                                        a: {
+                                                                            attributes: {
+                                                                                href: "lien/vers/qqch.html"
+                                                                            },
+                                                                            text: "Un lien"
+                                                                        }
+                                                                    }
+                                                                ]
+                                                            }
+                                                        },
+                                                        {
+                                                            li: {
+                                                                nodes: [
+                                                                    {
+                                                                        a: {
+                                                                            attributes: {
+                                                                                href: "lien/vers/qqch.html"
+                                                                            },
+                                                                            text: "Un autre lien"
+                                                                        }
+                                                                    }
+                                                                ]
+                                                            }
+                                                        },
+                                                        {
+                                                            li: {
+                                                                nodes: [
+                                                                    {
+                                                                        a: {
+                                                                            attributes: {
+                                                                                href: "lien/vers/qqch.html"
+                                                                            },
+                                                                            text: "Encore un"
+                                                                        }
+                                                                    }
+                                                                ]
+                                                            }
+                                                        },
+                                                        {
+                                                            li: {
+                                                                nodes: [
+                                                                    {
+                                                                        a: {
+                                                                            attributes: {
+                                                                                href: "lien/vers/qqch.html"
+                                                                            },
+                                                                            text: "Et un dernier"
+                                                                        }
+                                                                    }
+                                                                ]
+                                                            }
+                                                        }
+                                                    ]
+                                                }
+                                            }
+                                        ]
+                                    }
+                                },
+                                {
+                                    section: {
+                                        nodes: [
+                                            {
+                                                headerOne: {
+                                                    text: "Mon super site"
+                                                }
+                                            },
+                                            {
+                                                paragraph: {
+                                                    attributes: {
+                                                        id: "plop"
+                                                    },
+                                                    text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. A accusantium ad aliquid amet, architecto atque aut cum error facere in ipsa neque odio quasi quibusdam repellat unde vel, veritatis voluptates?"
+                                                }
+                                            }
+                                        ]
+                                    }
+                                },
+                                {
+                                    footer: {
+                                        nodes: [
+                                            {
+                                                paragraph: {
+                                                    text: "Copyright © 2019 - Mon site web"
+                                                }
+                                            }
+                                        ]
+                                    }
+                                }
+                            ]
+                        }
+                    }
+                ],
+            }
+        }
+    ]
 };
---
+++ 03-tree.js
@@ -0,0 +1,12 @@
+let section = document.querySelector('section');
+
+console.log("-----------------------------------");
+console.log("Afficher les enfants:");
+console.log(section.children);
+console.log("-----------------------------------");
+console.log("Afficher le parent :");
+console.log(section.parentNode);
+console.log("-----------------------------------");
+console.log("Afficher le premier enfant :");
+console.log(section.children[0]);
+console.log("-----------------------------------");
